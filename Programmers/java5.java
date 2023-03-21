00fe05595ce4e8484922f7a779b05f8145b02e71
ekgp209 <k2057217@naver.com>
2023-03-21 22:12:11 +0900

    230321_1문제

---
+++ Programmers/java5.java
@@ -0,0 +1,11 @@
+//피자 나눠먹기(2)
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        while((6*answer)%n != 0){
+            answer++;            
+        }
+        return answer;
+    }
+}
