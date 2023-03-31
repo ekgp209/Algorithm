f9cfde012064eaf88839fcdfc39d63f2b69e9cde
ekgp209 <k2057217@naver.com>
2023-04-01 04:08:40 +0900

    230401_3문제

--- Programmers/Lv0_10.java
+++ Programmers/Lv0_10.java
@@ -1,25 +1,8 @@
 // 진료순서 정하기
-class Solution1 {
-    public int[] solution(int[] emergency) {
-        
-        int[] array = new int[emergency.length]; //진료순서를 정할 배열
-        
-        //모두가 진료를 받기 때문에 1로 초기화
-        for(int i = 0; i<array.length; i++)
-            array[i] = 1; 
-        
-        //숫자끼리 비교하여 순서 정해줌. 이 부분에서 max를 이용해도 괜찮을 것 같음
-        for(int i = 0; i<emergency.length; i++){
-            for(int j = i+1; j<emergency.length; j++){
-                if(emergency[i] < emergency[j])
-                    array[i] += 1;
-            }
-        }
-    }
-}
-//collection의 reverseOrder()사용하여 내림차순 정렬
 import java.util.Arrays;
 import java.util.Collections;
+
+//collection의 reverseOrder()사용하여 내림차순 정렬
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
@@ -35,6 +18,19 @@ class Solution {
                 }
             }
         }
+
+
+        // //모두가 진료를 받기 때문에 1로 초기화
+        // for(int i = 0; i<answer.length; i++)
+        // answer[i] = 1; 
+    
+        // //숫자끼리 비교하여 순서 정해줌. 이 부분에서 max를 이용해도 괜찮을 것 같음
+        // for(int i = 0; i<emergency.length; i++){
+        //     for(int j = i+1; j<emergency.length; j++){
+        //         if(emergency[i] < emergency[j])
+        //             answer[i] += 1;
+        //     }
+        // }
         return answer;
     }
 }
