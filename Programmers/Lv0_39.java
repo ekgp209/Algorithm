c4e6d4da8e8723211fe93ee5e35074a1fa6a1940
ekgp209 <k2057217@naver.com>
2023-04-08 11:32:19 +0900

    230408_1문제

--- Programmers/Lv0_39.java
+++ Programmers/Lv0_39.java
@@ -1,40 +1,34 @@
-public class Lv0_39 {
-    public static String[] solution(String[] quiz) {
+// OX 퀴즈
+class Solution {
+    public String[] solution(String[] quiz) {
         /* 
-        1. 배열의 i번째를 가져와서 공백을 기준으로 새 배열에 담음
-        2. 배열 0,2번째를 1번째 부호로 계산 -> 배열 4번쨰랑 같다면 "O"        
+        1. 문자열 배열 quiz의 각 수식을 숫자와 연산자만 남겨 새로운 2차열 배열에 담음
+        2. 배열 0, 2번째를 1번째 부호로 계산 -> 배열 4번째랑 같다면 "O" / 다르다면 "X"        
         */
-        //String[] estimate = new String[5];
-
-        String[] answer = new String[quiz.length];
-        int idx = 0;
         
-        for(int i=0; i<quiz.length; i++){
-            String[] str = quiz[i].split(" ");
+        String[][] str_arr = new String[quiz.length][5];    //문자열 하나당 문자 5개 이므로
+        String[] answer = new String[quiz.length];
+
+        for(int i=0; i<quiz.length; i++){         
+            str_arr[i] = quiz[i].split(" ");
             
             int result = 0;
-            if(str[1].equals("+")){
-                result = Integer.parseInt(str[0]) + Integer.parseInt(str[2]);
-                if(result == Integer.parseInt(str[4])){
-                    answer[idx] = "O";
+            if(str_arr[i][1].equals("+")){
+                result = Integer.parseInt(str_arr[i][0]) + Integer.parseInt(str_arr[i][2]);
+                if(result == Integer.parseInt(str_arr[i][4])){
+                    answer[i] = "O";
                 }else{
-                    answer[idx] = "X";
+                    answer[i] = "X";
                 }
-            }else if(str[1].equals("-")){
-                result = Integer.parseInt(str[0]) - Integer.parseInt(str[2]);
-                if(result == Integer.parseInt(str[4])){
-                    answer[idx] = "O";
+            }else if(str_arr[i][1].equals("-")){
+                result = Integer.parseInt(str_arr[i][0]) - Integer.parseInt(str_arr[i][2]);
+                if(result == Integer.parseInt(str_arr[i][4])){
+                    answer[i] = "O";  
                 }else{
-                    answer[idx] = "X";
+                    answer[i] = "X";  
                 }
             }
-        }
-
+        }      
         return answer;
-    }
-    
-    public static void main(String[] args) {
-        String[] quiz = {"3 - 4 = -3", "5 + 6 = 11"};
-        Lv0_39.solution(quiz);
     }
 }
