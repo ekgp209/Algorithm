aba6c787c4ddc7fa7bc292001210d30beefc3fcc
ekgp209 <k2057217@naver.com>
2023-04-14 23:45:49 +0900

    230414_1문제

---
+++ Programmers/Lv0_44.java
@@ -0,0 +1,14 @@
+// 직사각형 넓이 구하기
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int width = 0;
+        int height = 0;
+        
+        for(int i=0; i<4; i++){
+            if()
+        }
+        
+        return answer;
+    }
+}
