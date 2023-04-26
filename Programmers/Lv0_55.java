0b1b57fdec71dbb7fd90f5fa051b979c5e1557bf
KDH <k2057217@naver.com>
2023-04-26 23:48:26 +0900

    230426_2문제

--- Programmers/Lv0_55.java
+++ Programmers/Lv0_55.java
@@ -1,13 +1,20 @@
 // k의 개수
 class Solution {
-    public int solution(int M, int N) {
+    public int solution(int i, int j, int k) {
         int answer = 0;
+        char ch_k = (char)(k + '0');            //비교를 위해 k를 char로 변경
         
-        if(M==1 && N==1) {
-            answer = 0;
-        }else {
-            answer = (M-1) + (N-1)*M;
+        for(int a=i; a<=j; a++){
+            String str = Integer.toString(a);   //숫자를 문자열로
+            char[] ch = str.toCharArray();      //문자열을 char배열로
+            
+            for(int b=0; b<ch.length; b++){
+                if(ch[b] == ch_k){              //각 자리 비교
+                    answer++;
+                }
+            }
         }
+        
         return answer;
     }
 }
