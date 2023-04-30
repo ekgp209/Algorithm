38797948eb5eeae9a56bb2fbcbe5e4e8731efa40
Dahye <k2057217@naver.com>
2023-04-30 23:42:16 +0900

    230430_1문제

---
+++ Programmers/Lv1_07.java
@@ -0,0 +1,12 @@
+// 짝수와 홀수
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num%2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
