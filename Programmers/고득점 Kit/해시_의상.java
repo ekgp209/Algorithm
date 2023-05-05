8bdae19f6a9cb013a6f7cd7ffc4d86a32513b19a
Dahye <k2057217@naver.com>
2023-05-05 10:19:53 +0900

    230505_1문제

---
+++ Programmers/고득점 Kit/해시_의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        //k:종류, v:개수 map에 추가
+        for(int i=0; i<clothes.length; i++){
+            if(map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);   //key의 값에 +1하여 개수 추가
+            }else{
+                map.put(clothes[i][1], 1);
+            }
+        }
+        
+        int answer = 1;
+        for(String key : map.keySet()){
+            answer *= map.get(key) + 1;     //안입는 경우도 있으므로 +1
+        }
+ 
+        return answer-1;    //아무것도 안입었을 경우는 제외
+    }
+}
