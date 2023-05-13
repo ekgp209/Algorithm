841d77941a5f3e385ddc892bc9f38d102c42665b
Dahye <k2057217@naver.com>
2023-05-13 23:27:34 +0900

    230513_1문제(다른방식)

---
+++ Programmers/Kit_08.java
@@ -0,0 +1,24 @@
+// 스택/큐. 같은 숫자는 싫어(ArrayList)
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+       List<Integer> list = new ArrayList<>();
+        
+        int tmp = -1;   //원소 0~9에 영향가지 않는 아무 숫자
+        
+        for(int n : arr){
+            if(tmp != n){
+                list.add(n);
+                tmp = n;
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
