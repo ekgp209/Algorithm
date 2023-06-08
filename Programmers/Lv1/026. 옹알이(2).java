c3732f30433c2453be09ed24f44a76e2c81903e3
Dahye <k2057217@naver.com>
2023-06-08 11:19:27 +0900

    230608_1문제

---
+++ Programmers/Lv1/026. 옹알이(2).java
@@ -0,0 +1,19 @@
+package Lv1;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] str = {"aya", "ye", "woo", "ma"};
+        String[] repeatStr = {"ayaaya", "yeye", "woowoo", "mama"};  //같은 단어가 반복될 경우
+        for(int i=0; i<babbling.length; i++){
+            for(int j=0; j<str.length; j++){
+                babbling[i] = babbling[i].replaceAll(repeatStr[j], "1").replaceAll(str[j], " ");
+            }
+            
+            if(babbling[i].trim().length() == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
