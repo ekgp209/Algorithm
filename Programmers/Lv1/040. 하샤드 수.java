ed09caf802ef4744ec68a5e4a31a59691c0fe814
Dahye <k2057217@naver.com>
2023-06-20 09:59:27 +0900

    1문제

---
+++ Programmers/Lv1/040. 하샤드 수.java
@@ -0,0 +1,20 @@
+package Lv1;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int tmp = x;
+        
+        while(tmp > 0){
+            sum += tmp%10;
+            tmp /= 10;                
+        }
+        
+        if(x % sum != 0){
+            return false;
+        }
+        
+        return answer;
+    }
+}
