91ea6f64b8fd3134b78a9cfc3c2e0f40040286bd
Dahye <k2057217@naver.com>
2023-06-22 21:20:23 +0900

    230622_2문제 수정

--- Programmers/Lv1/028. 문자열 나누기.java
+++ Programmers/Lv1/028. 문자열 나누기.java
@@ -1,5 +1,25 @@
 package Lv1;
 
-public class 028. 문자열 나누기 {
-    
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char first = s.charAt(0);
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            if(cnt1 == cnt2){
+                answer++;
+                first = s.charAt(i);
+                cnt1 = 0;
+                cnt2 = 0;
+            }                
+            if(s.charAt(i) == first){
+                cnt1++;                  
+            }else{
+                cnt2++;
+            }
+        }
+        return answer;
+    }
 }
--- Programmers/Lv1/029. 크레인 인형뽑기 게임.java
+++ Programmers/Lv1/029. 크레인 인형뽑기 게임.java
@@ -1,5 +1,26 @@
 package Lv1;
 
-public class 029. 크레인 인형뽑기 게임 {
-    
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;     //사라진 인형 수
+        List<Integer> basket = new ArrayList<>();
+        
+        for(int i=0; i<moves.length; i++){
+            for(int j=0; j<board.length; j++){
+                if(board[j][moves[i]-1] > 0){
+                    if((basket.size() > 0) && (basket.get(basket.size()-1) == board[j][moves[i]-1])){
+                        basket.remove(basket.size()-1);
+                        answer += 2;
+                    }else{
+                        basket.add(board[j][moves[i]-1]);
+                    }
+                    board[j][moves[i]-1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
 }
