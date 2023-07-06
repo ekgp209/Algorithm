e645fbfd4ef8aba2ce69144561ee62af39ba996c
Dahye <k2057217@naver.com>
2023-07-07 01:02:48 +0900

    1문제

--- Programmers/Lv0/083. 삼각형의 완성조건(2).java
+++ Programmers/Lv0/083. 삼각형의 완성조건(2).java
@@ -3,13 +3,38 @@ package Lv0;
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] sides) {
+    public int solution1(int[] sides) {
         int answer = 0;
         Arrays.sort(sides);  //가장 긴 변 설정
         
-        int sum = sides[0] + sides[1];
+        int min = sides[0];
+        int max = sides[1];
+        
+        //가장 긴 변이 max인 경우
+        for(int i=max-min+1; i<=max; i++){
+            answer++;
+        }
+        
+        //나머지 한 변이 가장 긴 변인 경우
+        for(int i=max+1; i<max+min; i++){
+            answer++;
+        }
+        
+        return answer;
+    }
+
+    public int solution2(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);  //가장 긴 변 설정
+        
+        int sum = sides[0] + sides[1];      
         for(int i=1; i<sum; i++){
-            if((sides[0]+i) > sides[1]){
+            //가장 긴 변이 sides[1]인 경우
+            if(sides[1] < sides[0]+i && i <= sides[1]){
+                answer++;
+            }
+            //나머지 한 변이 가장 긴 변인 경우
+            else if(sides[1] < i && i < sum){
                 answer++;
             }
         }
