ad8d7897eb67db425b096aeea2b499b037bc45f0
Dahye <k2057217@naver.com>
2023-08-01 19:38:53 +0900

    modify code

--- Programmers/Lv0/090. 등수 매기기.java
+++ Programmers/Lv0/090. 등수 매기기.java
@@ -1,5 +1,24 @@
 package Lv0;
 
-public class 90. 등수 매기기 {
-    
+class Solution {
+    public int[] solution(int[][] score) {        
+        int[] answer = new int[score.length];
+        
+        double[] avg = new double[score.length];    //각 학생의 평균 저장
+        for(int i=0; i<score.length; i++) {
+            avg[i] = (double)(score[i][0] + score[i][1]) / 2;
+        }
+
+        for(int i=0; i<avg.length; i++) {
+            int rank = 1;
+            for(int j=0; j<avg.length; j++) {
+                if(avg[i] < avg[j]) {
+                    rank++;
+                }
+            }
+            answer[i] = rank;
+        }
+        
+        return answer;
+    }
 }
