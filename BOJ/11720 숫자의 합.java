113be1ac43787c5e9412cb504923b3df983caf7e
Dahye <k2057217@naver.com>
2023-08-02 14:21:08 +0900

    230802_1문제

---
+++ BOJ/11720 숫자의 합.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+        String sNum = scan.next();
+        char[] cNum = sNum.toCharArray();
+        
+        int sum = 0;
+        for(int i=0; i<cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+        
+        System.out.println(sum);
+    }
+}
