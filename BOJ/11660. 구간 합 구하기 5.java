5f611d81cbf8d48a3aa7581bbf69403bbef4de6c
Dahye <k2057217@naver.com>
2023-08-06 19:34:27 +0900

    1문제

--- BOJ/11660. 구간 합 구하기 5.java
+++ BOJ/11660. 구간 합 구하기 5.java
@@ -13,24 +13,29 @@ class Main {
         int N = Integer.parseInt(st.nextToken());    //표의 크기
         int M = Integer.parseInt(st.nextToken());    //합을 구해야 하는 횟수
 
-        int[][] D = new int[N+1][N+1];
+        int[][] board = new int[N+1][N+1];
         //표의 값 입력받기
-        D[0][0] = 0;
+        board[0][0] = 0;
         for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
             for(int j=1; j<=N; j++){
                 int a = Integer.parseInt(st.nextToken());
-                D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + a;
+                board[i][j] = board[i][j-1] + board[i-1][j] - board[i-1][j-1] + a;
             }
         }
 
+        int answer = 0;
         for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            //구간 합 공식 (a ~ b까지 구간 합)
-            System.out.println(D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]);      
+            //구간 합 공식
+            answer = board[x2][y2] - board[x1-1][y2] - board[x2][y1-1] + board[x1-1][y1-1];
+
+            System.out.println(answer);      
         }
     }
 }
