142e23b698461ec342364f6a2b3a4078b7036f59
Dahye <k2057217@naver.com>
2023-08-07 12:06:40 +0900

    1문제

--- BOJ/2018. 수들의 합 5.java
+++ BOJ/2018. 수들의 합 5.java
@@ -1,5 +1,30 @@
 package BOJ;
 
-public class 2018. 수들의 합 5 {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 0, end = 0;     //투포인터 설정
+        int sum = 0, cnt = 0;
+        while(start <= N) {
+            //합이 N과 일치할 때
+            if(sum == N) {
+                cnt++;
+            }
+
+            if(sum < N) {
+                end++;
+                sum += end;
+            }else if(N <= sum) {
+                start++;
+                sum -= start;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
