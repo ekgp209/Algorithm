d59e2f5b5753a650c3226b31aa32d0b4b93c89f0
Dahye <k2057217@naver.com>
2023-08-31 17:15:46 +0900

    1문제

--- BOJ/Gold/1744. 수 묶기.java
+++ BOJ/Gold/1744. 수 묶기.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class P1744_수묶기 {
+class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
