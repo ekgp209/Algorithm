c5e3998ca107eb6e3292e363cd526774b7025815
Dahye <k2057217@naver.com>
2023-09-03 21:52:07 +0900

    1문제

---
+++ BOJ/Silver/1927. 최소 힙.java
@@ -0,0 +1,30 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());     //연산의 개수
+        for(int i=0; i<N; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            //큐가 비어있는데 출력해야 할 경우
+            if(pq.size() == 0 && x == 0){
+                System.out.println("0");
+                continue;
+            }
+
+            if(x != 0){
+                pq.add(x);
+            }else{
+                System.out.println(pq.poll());
+            }
+        }
+    }
+}
