2a855e8e31bbd9502687c1e6cfd49968aeede0fa
Dahye <k2057217@naver.com>
2023-09-17 20:03:19 +0900

    1문제

--- Programmers/Lv1/046. 부족한 금액 계산하기.java
+++ Programmers/Lv1/046. 부족한 금액 계산하기.java
@@ -2,11 +2,13 @@ package Lv1;
 
 class Solution {
     public long solution(int price, int money, int count) {
-        long answer = -1;
+        long tot_money = 0;
 
         for(int i=1; i<=count; i++){
-            int now_money = price*i;
+            long now_money = price*i;
+            tot_money += now_money;
         }
-        return answer;
+
+        return (money - tot_money >= 0) ? 0 : tot_money - money;
     }
 }
