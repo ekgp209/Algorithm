131fdc50b162cf67d1c251fe36f28446a08b510a
Dahye <49385606+ekgp209@users.noreply.github.com>
2023-09-27 15:47:24 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 12.36 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sArr = s.split(" ", -1);   //공백을 기준으로 자르되, 마지막 빈 값도 배열에 저장
+        
+        for(int i=0; i<sArr.length; i++){
+            for(int j=0; j<sArr[i].length(); j++){
+                char c = sArr[i].charAt(j);
+
+                if(j % 2 == 0){     //짝수 인덱스
+                    answer += Character.toUpperCase(c);
+                }else{              //홀수 인덱스
+                    answer += Character.toLowerCase(c);
+                }
+            }
+            
+            if(i != sArr.length-1){
+                answer += " ";  //단어 사이에 공백 추가
+            }
+        }
+        
+        return answer;
+    }
+}
