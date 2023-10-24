79f1ae96ab6c9a11bcd01f58639ae8c1c6bee8ca
Dahye <k2057217@naver.com>
2023-10-24 19:08:20 +0900

    12024. 숫자의 표현

---
+++ Programmers/Lv2/12024. 숫자의 표현.java
@@ -0,0 +1,22 @@
+package Lv2;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+            for(int j=i; j<=n;j++){
+                sum += j;
+
+                if(sum == n){
+                    answer++;
+                    break;
+                }
+
+                if(n < sum)     break;
+            }
+        }
+        return answer;
+    }
+}
