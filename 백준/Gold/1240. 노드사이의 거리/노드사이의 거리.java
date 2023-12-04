f98d61ec476cc3273ce0ee882fa64168a15b744c
Dahye <49385606+ekgp209@users.noreply.github.com>
2023-12-04 20:54:21 +0900

    [Gold V] Title: 노드사이의 거리, Time: 380 ms, Memory: 52740 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static ArrayList<Node>[] tree;
+    static boolean[] visited;
+    static int N;
+
+    static class Node{
+        int next;
+        int dist;
+
+        Node(int next, int dist){
+            this.next = next;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        N = Integer.parseInt(st.nextToken());       //노드 수
+        int M = Integer.parseInt(st.nextToken());   //노드쌍 수
+
+        tree = new ArrayList[N + 1];
+        for(int i=1; i<=N; i++){
+            tree[i] = new ArrayList<>();
+        }
+        for(int i=0; i<N-1; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            tree[s].add(new Node(e, w));
+            tree[e].add(new Node(s, w));
+        }
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            System.out.println(BFS(s, e));
+        }
+    }
+
+    static int BFS(int s, int e){
+        visited = new boolean[N + 1];
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(s, 0));
+        visited[s] = true;
+
+        int dist = 0;
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            //목적지에 도달했을 경우
+            if(node.next == e){
+                dist = node.dist;
+                break;
+            }
+
+            for(Node tmp : tree[node.next]){
+                if(!visited[tmp.next]){
+                    queue.add(new Node(tmp.next, node.dist + tmp.dist));
+                    visited[tmp.next] = true;
+                }
+            }
+        }
+        return dist;
+    }
+}
