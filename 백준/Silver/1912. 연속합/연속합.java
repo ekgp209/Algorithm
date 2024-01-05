7fef2d4591791cb4ac900bf0104957d374ca7fa7
Dahye <49385606+ekgp209@users.noreply.github.com>
2024-01-05 23:27:49 +0900

    [Silver II] Title: 연속합, Time: 260 ms, Memory: 24036 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] DP = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        DP[0] = arr[0];
+        int max = arr[0];
+        for(int i=1; i<n; i++){
+            DP[i] = Math.max(DP[i-1] + arr[i], arr[i]);
+
+            max = Math.max(max, DP[i]);
+        }
+        
+        System.out.println(max);
+    }
+}
