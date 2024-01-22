e0ad38be27dd76c661beaa0732e8ac0276411884
Dahye <49385606+ekgp209@users.noreply.github.com>
2024-01-22 10:45:01 +0900

    [Silver I] Title: 하노이 탑, Time: 444 ms, Memory: 52324 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main{
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /*
+         * 1. n-1개의 원판을 나머지 기둥으로 전부 옮긴다.
+         * 2. n번 원판을 목적지 기둥으로 옮긴다.
+         * 3. 나머지 n-1개의 원판을 목적지 기둥으로 옮긴다.
+         */
+        
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(new BigInteger("2").pow(N).subtract(new BigInteger("1")));    //옮긴횟수
+        if(N <= 20){
+            hanoi(N, 1, 2, 3);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    //hanoi(n(원반수), from(출발지), other(경유지), to(목적지))
+    static void hanoi(int n, int from, int other, int to) throws IOException{
+        if(n == 1) {
+            bw.write(from + " " + to + "\n");
+            return;
+        }
+
+        hanoi(n-1, from, to, other);        //1. 1번기둥의 n-1개를 3번을 경유해 2번으로 이동
+        
+        bw.write(from + " " + to + "\n");   //2. n번 원판을 3번으로 이동
+        
+        hanoi(n-1, other, from, to);    //3. 2번기둥의 n-1개를 1번을 경유해 3번으로 이동
+    }
+}
